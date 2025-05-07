181e87f496ad6bb8fe1f15b1e935f94f7e0759c0
ngerzson <ngerzson@gmail.com>
2025-05-07 21:09:58 +0300

    first commit

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+import requests
+import selenium
+
+response = requests.get("https://example.com")
+print(response.status_code)
+print(response.text)
+print("Hello, World!")
+print("This is a test file.")
